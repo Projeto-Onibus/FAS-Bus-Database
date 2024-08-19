b496a53fa4df59924ccbd6cf9ea5b86001bef3fc
Fernando Dias <fernandodias@gta.ufrj.br>
2024-08-19 18:19:10 -0300

    Created custom image that autoload empty database structure

--- database_struct.sql
+++ database_struct.sql
@@ -1,9 +1,31 @@
+-- Database for bus data
+CREATE DATABASE buses;
+CREATE USER buses WITH ENCRYPTED PASSWORD 'buses';
+\connect buses
 CREATE TABLE bus_trajectories (
     time_detection TIMESTAMPTZ NOT NULL,
     bus_id CHAR(10) NOT NULL,
+    line CHAR(6),
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL
 );
-
 SELECT create_hypertable('bus_trajectories','time_detection',chunk_time_interval => INTERVAL '1 day');
 CREATE UNIQUE INDEX idx_bus_id ON bus_trajectories(bus_id,time_detection);
+GRANT ALL PRIVILEGES ON DATABASE buses TO buses;
+GRANT CREATE ON SCHEMA public TO buses;
+GRANT ALL PRIVILEGES ON TABLE bus_trajectories TO buses;
+
+-- Database for line data
+CREATE DATABASE lines;
+CREATE USER lines WITH ENCRYPTED PASSWORD 'lines';
+\connect lines
+CREATE TABLE line_trajectories (
+    name CHAR(10) NOT NULL,
+    direction INTEGER NOT NULL,
+    position INTEGER NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
+);
+GRANT ALL PRIVILEGES ON DATABASE lines TO lines;
+GRANT CREATE ON SCHEMA public TO lines;
+GRANT ALL PRIVILEGES ON TABLE line_trajectories  TO lines;

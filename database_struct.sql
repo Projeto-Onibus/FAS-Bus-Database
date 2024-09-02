3e11d195f46d6fd38003aeccc59a8112405ab39b
Fernando Dias <fernando.dias.3741@gmail.com>
2024-09-01 22:24:43 -0300

    Added raw table to receive data through copy

--- database_struct.sql
+++ database_struct.sql
@@ -5,15 +5,23 @@ CREATE USER buses WITH ENCRYPTED PASSWORD 'buses';
 CREATE TABLE bus_trajectories (
     time_detection TIMESTAMPTZ NOT NULL,
     bus_id CHAR(10) NOT NULL,
-    line CHAR(6),
+    line CHAR(8),
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL
+);
+CREATE TABLE bus_trajectories_raw (
+    time_detection TIMESTAMPTZ,
+    bus_id CHAR(10),
+    line CHAR(8),
+    latitude FLOAT,
+    longitude FLOAT
 );
 SELECT create_hypertable('bus_trajectories','time_detection',chunk_time_interval => INTERVAL '1 day');
 CREATE UNIQUE INDEX idx_bus_id ON bus_trajectories(bus_id,time_detection);
 GRANT ALL PRIVILEGES ON DATABASE buses TO buses;
 GRANT CREATE ON SCHEMA public TO buses;
 GRANT ALL PRIVILEGES ON TABLE bus_trajectories TO buses;
+GRANT ALL PRIVILEGES ON TABLE bus_trajectories_raw TO buses;
 
 -- Database for line data
 CREATE DATABASE lines;
